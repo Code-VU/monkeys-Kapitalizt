3557bc1cf63103313a8e5a93ea9bcd64c8a45758
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-12 14:37:51 -0500

    2nd

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -8,13 +8,10 @@ def calculateTime():
         print("Uh Oh! We're in trouble!")
     else:
         print("Yay! We're going to have a good day!")
-    
-
-
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateTime() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-calculateTime()
+# calculateTime()
