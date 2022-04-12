69f715aa783ff3496f8054ea81b1ead20fdf0996
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-12 14:37:10 -0500

    1st

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,10 +4,17 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    if (monkey_one == 'y' and monkey_two == 'y') or (monkey_one == 'n' and monkey_two == 'n'):
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
+    
+
+
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateTime() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculateTime()
+calculateTime()
